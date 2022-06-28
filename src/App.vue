a0367e598131a9b09cdb4a4f63ddaa7076b338dd
ZamarShoo <shg.ivan97@gmail.com>
2022-06-28 15:26:29 +0300

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="house">
+  <div class="floor-left">
+    <div class="elevator"></div>
+    <div
+      v-for="n in numberFloors"
+      :key="n" 
+      class="floor">
+    </div>
+  </div>
+  <div class="floor-right">
+    <div
+      v-for="n in numberFloors"
+      :key="n" 
+      class="floor">
+        <p class="floor-number">{{n}}</p>
+        <p class="lift-call-button"></p>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from 'vuex'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    ...mapState({
+      numberFloors: state => state.numberFloors
+    }),
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .house {
+    display: flex;
+  }
+  .floor-left {
+    width: 10%;
+
+    .elevator {
+      position: absolute;
+      width: 100%;
+      height: 19vh;
+      background-color: tomato;
+      z-index: 1;
+      left: 0;
+      bottom: calc(0vh + 1px);
+      transition: 1s;
+    }
+  }
+  .floor-right {
+    width: 90%;
+    border-left: 1px solid black;
+  }
+  .floor-right, .floor-left {
+    display: flex;
+    flex-direction: column-reverse;
+    position: relative;
+    
+  }
+  .floor {
+    position: relative;
+    z-index: 2;
+    height: 19vh;
+    border-bottom: 1px solid black;
+  }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    numberFloors: 5,
+    currentFloor: 0,
+    turnFloors: [],
+    elevatorIsFree: true,
   },
   mutations: {
+    addToTurn(state, floor) {
+      state.turnFloors.push(floor)
+    },
+    inCurrentFloor(state) {
+      state.turnFloors.shift()
+    }
   },
   actions: {
+
   },
-  modules: {
-  }
 })
